bb36a37d92eee83d0a870d30036c1d04de696c29
rsafitri <safitri@campus.tu-berlin.de>
2023-08-12 22:52:15 +0200

    issue #2 resolved, can upload 1 gif

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //#region Globale Hilfsvariablen
-
 const maxImages = 3;   // Max 3 Bildern hochladen
 
 var displayWidth = 0;  // Breite der Anzeige von Server
@@ -18,7 +17,7 @@ function onPageLoad() {
   // Beim Laden der Seite wird eine GET SIZE Request gesendet
   // der Server schickt dann als Response die Größe der LED-Anzeige zurück
   // Bei einem Fehler wird der Nutzer benachrichtigt
-  /*fetch('./size')
+  fetch('./size')
     .then(response => response.json())
     .then(data => {
       displayWidth = data.size[0];
@@ -30,9 +29,7 @@ function onPageLoad() {
       displayWidth = 64;
       displayHeight = 32;
       alert("Es ist ein Fehler passiert! Bitte laden Sie die Seite neu!")
-    });*/
-  displayWidth = 64;
-  displayHeight = 32;
+    });
 }
 
 //#region Element transitionTime
@@ -41,7 +38,7 @@ const minDelay = parseInt(transitionTimeField.getAttribute('min'));
 const maxDelay = parseInt(transitionTimeField.getAttribute('max'));
 transitionTimeField.addEventListener("input", (e) => {
   // bei jeder Eingabe wird die Zahl geprüft, ob diese im Wertebereich liegt
-  var frameDelay = transitionTimeField.value;
+  const frameDelay = transitionTimeField.value;
 
   if (isNaN(frameDelay) || frameDelay > maxDelay || frameDelay < minDelay) {
     transitionTimeField.setCustomValidity("Bitte geben Sie Ganzzahlen zwischen " + minDelay + " und " + maxDelay + " ein!");
@@ -66,7 +63,7 @@ var textSendField = document.getElementById("textToSend");
 textSendField.addEventListener("input", () => {
   // bei jeder Texteingabe wird die Eingabe ohne führende Leerzeichen
   // und ohne Leerzeichen am Ende geprüft
-  var textToSend = textSendField.value.trim();
+  const textToSend = textSendField.value.trim();
   if (textToSend.length > 0)
     isValidText = true; // gültiger Text
   else
@@ -81,7 +78,7 @@ colorPicker.addEventListener("change", () => {
   // bei einer Änderung der ausgewählten Farbe wird die ausgewählte Farbe
   // in RGB als eine Variable gespeichert und geprüft, schwarz ist unzulässig
   // Anzeige schwarz (0,0,0) = LED aus
-  var rgbValue = hexToRGB(colorPicker.value);
+  const rgbValue = hexToRGB(colorPicker.value);
   if (rgbValue[0] == 0 && rgbValue[1] == 0 && rgbValue[2] == 0)
     isValidColor = false; // schwarz, Farbe ungültig
   else
@@ -158,14 +155,14 @@ function prepareSendImg() {
     // es gibt mehrere Bilder, das C Code Array von den Bildern mit der Größe und Delay in JSON Format wird als HTTP POST Request an API endpoint /movingimages gesendet
     // Bei einer Response wird diese als Meldung angezeigt, danach wird die Seite zurückgesetzt
     processImages()
-      .then((imageWithDelay) => {
-        console.log(JSON.stringify(imageWithDelay));
+      .then((imagesWithDelay) => {
+        console.log(JSON.stringify(imagesWithDelay));
         fetch('./movingimages', {
           method: 'POST',
           headers: {
             "Content-Type": "application/json"
           },
-          body: JSON.stringify(imageWithDelay)
+          body: JSON.stringify(imagesWithDelay)
         })
           .then(response => response.text())
           .then(data => {
@@ -210,14 +207,14 @@ function prepareSendImg() {
       // es gibt nur ein Bild, das C Code Array vom Bild mit der Größe in JSON Format wird als HTTP POST Request an API endpoint /image gesendet
       // Bei einer Response wird diese als Meldung angezeigt, danach wird die Seite zurückgesetzt
       processImg(uploadedImages[0])
-        .then(cCode => {
-          console.log(JSON.stringify(cCode));
+        .then(cCodeWithSize => {
+          console.log(JSON.stringify(cCodeWithSize));
           fetch('./image', {
             method: 'POST',
             headers: {
               "Content-Type": "application/json"
             },
-            body: JSON.stringify(cCode)
+            body: JSON.stringify(cCodeWithSize)
           })
             .then(response => response.text())
             .then(data => {
@@ -243,7 +240,7 @@ function prepareSendTxt() {
   // Beim Klick des Buttons "Text Hochladen" wird der eingegebene Text
   // mit der kompletten Einstellung in JSON Format als HTTP POST Request gesendet
   // Bei einer Response wird diese als Meldung angezeigt, danach wird die Seite zurückgesetzt
-  var textSetup = {
+  const textSetup = {
     value: textSendField.value,
     color: hexToRGB(colorPicker.value),
     mode: textModeField.value
@@ -287,7 +284,7 @@ function loadImage(files) {
       }
       else {
         if (files[i].type === 'image/gif') {
-          // eine .GIF Datei, darf nur einzeln hochgeladen werden
+          // eine .GIF Datei darf nur einzeln hochgeladen werden
           if (uploadedImages.length > 0) {
             alert("Eine .GIF Datei kann nur einzeln hochgeladen werden!");
             continue;
@@ -379,56 +376,57 @@ function loadImage(files) {
 
 async function processImages() {
   // Bilder und Delay in JSON Format vorbereiten
-  var imageWithDelay = {
+  const imagesWithDelay = {
     delay: transitionTimeField.value,
     images: []
   };
 
   for (let i = 0; i < uploadedImages.length; i++) {
     try {
+      // jedes Bild in C Code Array umwandeln
       const cCodeSize = await processImg(uploadedImages[i]);
-      imageWithDelay.images.push(cCodeSize);
+      imagesWithDelay.images.push(cCodeSize);
     }
     catch (error) {
       alert('Error: ' + error);
     }
   }
 
-  return imageWithDelay;
+  return imagesWithDelay;
 }
 
 function processGif() {
   // GIF Frames und Delay in JSON Format vorbereiten
-  var frameWithDelay = {
+  const framesWithDelay = {
     delays: [],
     frames: []
   };
 
-  // Gif laden
+  // Gif laden und konvertiert jede Frames in C Code Array sowie stellt Delays zwischen den Frames fest
   return new Promise((resolve, reject) => {
     // konvertiert das Bild in ein C Code Array
-    var reader = new FileReader();
+    const reader = new FileReader();
     reader.onload = async function (e) {
       const gifData = new Uint8Array(e.target.result);
-      const gif = new GifReader(gifData);
+      const gifReader = new GifReader(gifData);
 
-      for (let i = 0; i < gif.numFrames(); i++) {
-        const frame = gif.frameInfo(i);
-        frameWithDelay.delays.push(frame.delay * 10);
+      for (let i = 0; i < gifReader.numFrames(); i++) {
+        const frameInfo = gifReader.frameInfo(i);
+        framesWithDelay.delays.push(frameInfo.delay * 10);
 
-        // Decode the frame's pixels
-        const frameImageData = new Uint8Array(frame.width * frame.height * 4);
-        gif.decodeAndBlitFrameRGBA(i, frameImageData);
+        const image = new ImageData(frameInfo.width, frameInfo.height);
+        gifReader.decodeAndBlitFrameRGBA(i, image.data);
 
-        const imageElement = document.createElement('img');
-        imageElement.width = frame.width;
-        imageElement.height = frame.height;
-        imageElement.src = 'data:image/gif;base64,' + btoa(String.fromCharCode.apply(null, frameImageData));
-        const cCodeSize = convertImgToCCodeSize(imageElement);
-        frameWithDelay.frames.push(cCodeSize);
+        let canvas = document.createElement('canvas');
+        canvas.width = frameInfo.width;
+        canvas.height = frameInfo.height;
+        canvas.getContext('2d').putImageData(image, 0, 0);
+
+        const cCodeSize = convertImgToCCodeSize(canvas);
+        framesWithDelay.frames.push(cCodeSize);
       }
 
-      resolve(frameWithDelay);
+      resolve(framesWithDelay);
     };
     reader.readAsArrayBuffer(uploadedImages[0]);
     reader.onerror = (error) => reject(error);
@@ -438,10 +436,10 @@ function processGif() {
 function processImg(file) {
   return new Promise((resolve, reject) => {
     // konvertiert das Bild in ein C Code Array
-    var reader = new FileReader();
+    const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function () {
-      var image = new Image();
+      let image = new Image();
       image.src = reader.result;
       image.onload = (e) => resolve(convertImgToCCodeSize(e.target));
     };
@@ -450,10 +448,10 @@ function processImg(file) {
 }
 
 function convertImgToCCodeSize(image) {
-  var canvas = document.createElement('canvas');
+  let canvas = document.createElement('canvas');
   // Bild skalieren wenn nötig
   if (image.width > displayWidth || image.height > displayHeight) {
-    var scale = Math.min(displayWidth / image.width, displayHeight / image.height);
+    let scale = Math.min(displayWidth / image.width, displayHeight / image.height);
 
     canvas.width = image.width * scale;
     canvas.height = image.height * scale;
@@ -464,11 +462,11 @@ function convertImgToCCodeSize(image) {
   }
 
   // Bild für die LED-Anzeige verarbeiten
-  var context = canvas.getContext('2d');
+  let context = canvas.getContext('2d');
   context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-  var pixelData = imageData.data;
+  let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  let pixelData = imageData.data;
 
   // pixel data berechnen für das C Code Array, es wird mit der Größe des Bilds in JSON Format vorbereitet 
   const cCodeSize = {
@@ -477,12 +475,12 @@ function convertImgToCCodeSize(image) {
   };
 
   for (var i = 0; i < pixelData.length; i += 4) {
-    var r = pixelData[i];
-    var g = pixelData[i + 1];
-    var b = pixelData[i + 2];
+    let r = pixelData[i];
+    let g = pixelData[i + 1];
+    let b = pixelData[i + 2];
 
-    var uint16Value = ((r & 0x1F) << 11) | ((g & 0x3F) << 5) | (b & 0x1F);
-    var hexValue = uint16Value.toString(16).toUpperCase().padStart(4, '0');
+    let uint16Value = ((r & 0x1F) << 11) | ((g & 0x3F) << 5) | (b & 0x1F);
+    let hexValue = uint16Value.toString(16).toUpperCase().padStart(4, '0');
     cCodeSize.hexValues.push('0x' + hexValue);
   }
 
